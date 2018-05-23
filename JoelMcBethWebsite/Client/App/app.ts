61208af4eddffa3dd73e79af7ca6337024274616
Joel McBeth <joel.mcbeth@gmail.com>
2018-05-22 22:11:23 -0400

    Trying for the 30th time to get module loading to work with Typescript.

--- JoelMcBethWebsite/Client/App/app.ts
+++ JoelMcBethWebsite/Client/App/app.ts
@@ -1,1 +1,12 @@
-﻿
+﻿import * as $ from "jquery"
+import * as sammy from "sammy"
+
+var app = Sammy();
+
+app.get('#/', context => {
+    context.log("index page");
+});
+
+$(() => {
+    app.run();
+});
