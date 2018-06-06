0cba1b800f4d358d3164f7731328515fb8efd263
Joel McBeth <joel.mcbeth@gmail.com>
2018-06-05 22:45:35 -0400

    Added routing.

--- JoelMcBethWebsite/wwwroot/js/app.js
+++ JoelMcBethWebsite/wwwroot/js/app.js
@@ -1,7 +1,18 @@
 /// <reference path="angular.min.js" />
+/// <reference path="angular-route.js" />
 
-var app = angular.module('app', []);
+
+var app = angular.module('app', ["ngRoute"]);
 
 app.controller('mainCtrl', function ($scope) {
     $scope.text = 'Hello world';
+});
+
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: 'home.html'
+    })
+    .when("/books", {
+        templateUrl: 'books.html'
+    });
 });
