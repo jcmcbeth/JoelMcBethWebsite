d0d543b1bf43722911de8826f9349bceb8b83228
jcmcbeth <joel.mcbeth@gmail.com>
2018-06-07 21:30:45 -0400

    Refactored to better follow style guidelines

--- JoelMcBethWebsite/wwwroot/js/app.js
+++ JoelMcBethWebsite/wwwroot/js/app.js
@@ -1,44 +1,101 @@
 /// <reference path="angular.min.js" />
 /// <reference path="angular-route.js" />
 
+// app.module.js
 
-var app = angular.module('app', ["ngRoute"]);
+(function () {
+    angular.module('app', ["ngRoute"]);
+})();
 
-app.controller('mainCtrl', function ($scope) {
-    $scope.text = 'Hello world';
-});
 
-app.controller('bookCtrl', function ($scope, bookService) {
-    bookService.getBooks()
-        .then(function (response) {
-            $scope.books = response.data;
+// book.controller.js
+
+(function () {
+    angular
+        .module("app")
+        .controller("BookController", BookController);
+
+    BookController.$inject = ["BookService"];
+
+    function BookController(bookService) {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            return bookService.getBooks().then(function (books) {
+                vm.books = books;
+            });
+        }
+    }
+})();
+
+// resume.controller.js
+
+(function () {
+    angular
+        .module("app")
+        .controller("ResumeController", ResumeController);
+
+    ResumeController.$inject = ["$http"];
+
+    function ResumeController($http) {
+        var vm = this;
+
+        activate();
+
+        function activate() {
+            return $http.get("resume.json").then(function (response) {
+                vm.resume = response.data;
+            });
+        }
+    }
+})();
+
+// app.routes.js
+
+(function () {
+    angular
+        .module("app")
+        .config(configure);
+
+    function configure($routeProvider, $locationProvider) {
+        $routeProvider.when("/", {
+            templateUrl: "home.html"
+        })
+        .when("/books", {
+            templateUrl: "books.html",
+            controller: "BookController",
+            controllerAs: "vm"
+        })
+        .when("/resume", {
+            templateUrl: "resume.html",
+            controller: "ResumeController",
+            controllerAs: "vm"
         });
-});
+    }
+})();
 
-app.controller('resumeCtrl', function ($scope, $http) {
-    $http.get("resume.json")
-        .then(function (response) {
-            $scope.resume = response.data;
-        });
-});
+// book.service.js
 
-app.config(function ($routeProvider, $locationProvider) {
+(function () {
+    angular
+        .module("app")
+        .factory("BookService", BookService);
 
-    $routeProvider.when('/', {
-        templateUrl: 'home.html'
-    })
-    .when('/books', {
-        templateUrl: 'books.html',
-        controller: 'bookCtrl'
-    })
-    .when("/resume", {
-        templateUrl: 'resume.html',
-        controller: 'resumeCtrl'
-    });
-});
+    BookService.$inject = ["$http"];
 
-app.service("bookService", function ($http) {
-    this.getBooks = function () {
-        return $http.get("/api/books");
-    };   
-});
+    function BookService($http) {
+        return {
+            getBooks: getBooks
+        };
+
+        function getBooks() {
+            return $http.get("/api/books").then(getBooksComplete);
+
+            function getBooksComplete(response) {
+                return response.data;
+            }
+        }
+    }
+})();
