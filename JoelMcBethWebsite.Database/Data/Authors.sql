3b44f0aedba811114379eb75135dc850e24fda69
jcmcbeth <joel.mcbeth@gmail.com>
2018-11-03 14:04:37 -0400

    Entered in legitimate book data.

--- JoelMcBethWebsite.Database/Data/Authors.sql
+++ JoelMcBethWebsite.Database/Data/Authors.sql
@@ -2,7 +2,40 @@
 
 MERGE INTO [Authors] AS [Target]
 USING (VALUES
-	(1, 'John', 'Sonmez', NULL)
+	(1, 'Armin A.', 'Brott', 'A.'),
+	(2, 'Gary', 'Greenberg', ''),
+	(3, 'Joel', 'Grus', ''),
+	(4, 'Robert', 'Martin', 'C.'),
+	(5, 'Martin', 'Sternberg', ''),
+	(6, 'Wenbo', 'Mao', ''),
+	(7, 'Andrew', 'Hunt', ''),
+	(8, 'David', 'Thomas', ''),
+	(9, 'Robert', 'Faludi', ''),
+	(10, 'Daniel', 'Kalla', ''),
+	(11, 'Jeff', 'Duntemann', ''),
+	(12, 'Aron', 'Ralston', ''),
+	(13, 'Donald', 'Knuth', 'E.'),
+	(14, 'Kyle', 'Baley', ''),
+	(15, 'Donald', 'Belcham', ''),
+	(16, 'John', 'Derbyshire', ''),
+	(17, 'Charles', 'Platt', ''),
+	(18, 'Michael', 'Feathers', ''),		
+	(19, 'Mark', 'Seemann', ''),
+	(20, 'Roy', 'Osherove', ''),
+	(21, 'Francesco', 'Balena', ''),
+	(22, 'Giuseppe', 'Dimauro', ''),
+	(23, 'Bruce', 'Payette', ''),		
+	(24, 'Marcin', 'Kawalerowicz', ''),
+	(25, 'Craig', 'Berntson', ''),
+	(26, 'William', 'Bartholomew', ''),
+	(27, 'Sayed', 'Hashimi', ''),
+	(28, 'Nick', 'Ramirez', ''),
+	(29, 'Jennifer', 'Gunter', ''),
+	(30, 'Gary', 'Ezzo', ''),
+	(31, 'Robert', 'Bucknam', ''),
+	(32, 'Dale', 'Carnagie', ''),
+	(33, 'Coyote', 'Peterson', ''),
+	(34, 'John', 'Sonmez', '')
 ) AS [Source] ([Id], [FirstName], [LastName], [MiddleName])
 ON
 	([Target].[Id] = [Source].[Id])
