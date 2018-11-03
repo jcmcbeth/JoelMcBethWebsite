a1dbc9838e53c48148c2bcae53117713b81ccd34
jcmcbeth <joel.mcbeth@gmail.com>
2018-11-02 20:12:36 -0400

    Added loading glyph for the main page.

---
+++ JoelMcBethWebsite/wwwroot/app/app.controller.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AppController', AppController);
+
+    AppController.$inject = [];
+
+    function AppController() {
+        /* jshint validthis:true */
+        var vm = this;
+
+        vm.loaded = false;
+        vm.initialize = initialize;
+
+        activate();
+
+        function activate() {
+        }
+
+        function initialize() {
+            vm.loaded = true;
+        }
+    }
+})();
