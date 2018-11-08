33471309bcc195ffee89fd0301fb65b084bcfccf
jcmcbeth <joel.mcbeth@gmail.com>
2018-11-07 19:56:52 -0500

    Beginning service design for resources.

---
+++ JoelMcBethWebsite/wwwroot/app/services/resource.service.js
@@ -0,0 +1,46 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('resourceService', resourceService);
+
+    resourceService.$inject = ["$q"];
+
+    function resourceService($q) {
+        var service = {
+            getResources: getResources,
+            getResourceGroups: getResourceGroups
+        };
+
+        return service;
+
+        function getResourceGroups() {
+            var groups = [
+                {
+                    name: "Applications",
+                    description: "",
+                    subgroups: [
+                        {
+                            name: "Tools"
+                        }
+                    ]
+                }
+            ];
+
+            return $q(groups);
+        }
+
+        function getResources() {
+            var resources = [
+                {
+                    url: "",
+                    title: "",
+                    description: ""
+                }
+            ];
+
+            return $q(resources);
+        }
+    }
+})();
