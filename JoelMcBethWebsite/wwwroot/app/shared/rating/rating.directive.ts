beb511785c4c1f40b0613dd3ab032111c99c50c0
jcmcbeth <joel.mcbeth@gmail.com>
2019-01-17 20:49:57 -0500

    Changed rating directive to use classes.

---
+++ JoelMcBethWebsite/wwwroot/app/shared/rating/rating.directive.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../../../../client/typings/angularjs/index.d.ts" />
+
+class RatingDirective implements ng.IDirective {
+    public scope = {
+        value: "<"
+    };
+    public restrict = "E";    
+    public controller = "RatingController as $ctrl";
+    public bindToController = true;
+    public templateUrl = "/app/shared/rating/rating.html";
+    public css = "/app/shared/rating/rating.css";
+
+    static getFactory(): ng.IDirectiveFactory {
+        const directive = () => new RatingDirective();
+
+        return directive;
+    }
+}
+
+angular
+    .module('app')
+    .directive('rating', RatingDirective.getFactory());
