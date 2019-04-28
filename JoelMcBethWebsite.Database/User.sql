e1587b206bb9a6852809c9d8bc3ccb6a31f0e4e5
jcmcbeth <joel.mcbeth@gmail.com>
2019-04-28 13:30:06 -0400

    Put s on wrong column.

--- JoelMcBethWebsite.Database/User.sql
+++ JoelMcBethWebsite.Database/User.sql
@@ -6,7 +6,7 @@
     [HashedPassword] BINARY(24) NULL, 
     [PasswordSalt] BINARY(24) NULL, 
     [IsApproved] BIT NOT NULL DEFAULT 0, 
-    [FailedLoginAttempt] INT NOT NULL, 
+    [FailedLoginAttempts] INT NOT NULL, 
     [IsLocked] BIT NOT NULL, 
-    [LastLoginAttempts] DATETIME NULL
+    [LastLoginAttempt] DATETIME NULL
 )
